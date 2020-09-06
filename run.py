38e81a2e02c5b90fe59ba071a5a70feac6f0a1d7
legionpunisher <mbuguamoses656@gmail.com>
2020-09-06 23:24:42 +0300

    created a function called create_user()

--- run.py
+++ run.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3.8
+from user import User
+from credentials import Credential
+def create_user(fname,lname,phone,email):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(fname,lname,phone,email)
+    return new_user
