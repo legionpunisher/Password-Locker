ef4c2578f9dd95fed93c307ebb66286085c8c32d
legionpunisher <mbuguamoses656@gmail.com>
2020-09-06 15:26:01 +0300

    modified the indentation on the self Test User

--- user_test.py
+++ user_test.py
@@ -21,7 +21,7 @@ class TestUser (unittest.TestCase):
         self.assertEqual(self.new_user.phone_number,"0790437510")
         self.assertEqual(self.new_user.email,"mbuguamoses656@gmail.com")
         
-    def test_save_user(self):
+    def test_save_user (self):
         """
         test_save_user test case to test if the user object is saved into the user list
         """
