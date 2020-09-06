666514df4e072be3268cbc7d490780e0b4c92bcb
legionpunisher <mbuguamoses656@gmail.com>
2020-09-06 14:26:04 +0300

    passed properties to our objects created an empty list to store our user objects

--- user.py
+++ user.py
@@ -2,4 +2,11 @@ class User:
     """
     class that generates new instance of user details
     """
-    pass
+    user_details = []
+    
+    def _init_(self,first_name,last_name,number,email):
+    
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = number
+        self.email= email
