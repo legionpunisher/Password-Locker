6e3dc26fcd8094f6b8e10b584753196fe3db0d21
legionpunisher <mbuguamoses656@gmail.com>
2020-09-06 17:12:12 +0300

    addded an account property in our credentials object

--- credentials.py
+++ credentials.py
@@ -3,6 +3,7 @@ class Credential:
     Class that generates new instances of credentials
     """
     credential_list = []
-    def __init__(self,username,password):
+    def __init__(self,username,password,account):
         self.username = username
         self.password = password
+        self.account = account
