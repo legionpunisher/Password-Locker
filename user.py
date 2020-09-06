22059bbee6bcfaf207f15233fcb7f746929bdaa3
legionpunisher <mbuguamoses656@gmail.com>
2020-09-06 16:39:18 +0300

    created a delete test and a delete functionality to our user test

--- user.py
+++ user.py
@@ -10,9 +10,19 @@ class User:
         self.last_name = last_name
         self.phone_number = number
         self.email= email
-    
+     
+    #A function to save user
+     
     def save_user(self):
         """
         save_user method saves user objects into user_list
         """
         User.user_list.append(self)
+        
+    # a function to delete user
+    def  delete_user(self):
+        '''
+        delete_user method deletes a saved user from the user_list
+        '''
+        User.user_list.remove(self)
+    
--- user_test.py
+++ user_test.py
@@ -30,13 +30,13 @@ class TestUser (unittest.TestCase):
         self.assertEqual(len(User.user_list),1)  
 
     
-    def tearDown(self):
+    def tearDown (self):
         '''
         tearDown method that does clean up after each test case has run.
         '''
         User.user_list = []
 
-    def test_save_multiple_user(self):
+    def test_save_multiple_user (self):
         '''
         test_save_multiple_user to check if we can save multiple user
         objects to our user_list
@@ -45,7 +45,19 @@ class TestUser (unittest.TestCase):
         test_user = User("Test","user","074040404","test@user.com") # new user
         test_user.save_user()
         self.assertEqual(len(User.user_list),2)
-
+        
+    #fourth test
+    
+    def test_delete_user(self):
+        '''
+        test_delete_user to test if we can remove a user from our user list
+        '''
+        self.new_user.save_user()
+        test_user = User("Test","user","074040404","test@user.com")
+        test_user.save_user()  
+                       
+        self.new_user.delete_user()
+        self.assertEqual(len(User.user_list),1)        
 if __name__ == '__main__':
     unittest.main()
 
