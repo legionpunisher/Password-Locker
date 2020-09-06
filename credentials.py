5b4468cff40d228ca789473d50008caf2ac0a4f7
legionpunisher <mbuguamoses656@gmail.com>
2020-09-06 14:32:00 +0300

    created a credential list to store our created credential object

--- credentials.py
+++ credentials.py
@@ -2,6 +2,7 @@ class Credential:
     """
     Class that generates new instances of credentials
     """
+    credential_list = []
     def __init__(self,username,password):
         self.username = username
         self.password = password
