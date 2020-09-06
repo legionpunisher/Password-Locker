c46e2000a8333265239b0a5843b1bee60048b072
legionpunisher <mbuguamoses656@gmail.com>
2020-09-06 15:49:37 +0300

    added a tear down method to execute a set of new instructions after every test this helps us save multiple contacts

--- user_test.py
+++ user_test.py
@@ -21,13 +21,33 @@ class TestUser (unittest.TestCase):
         self.assertEqual(self.new_user.phone_number,"0790437510")
         self.assertEqual(self.new_user.email,"mbuguamoses656@gmail.com")
         
+    #Second Test
     def test_save_user (self):
         """
         test_save_user test case to test if the user object is saved into the user list
         """
         self.new_user.save_user()
-        self.assertEqual(len(User.user_list),1)
-        
+        self.assertEqual(len(User.user_list),1)  
+
+    
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        User.user_list = []
+
+    def test_save_multiple_user(self):
+        '''
+        test_save_multiple_user to check if we can save multiple user
+        objects to our user_list
+        '''
+        self.new_user.save_user()
+        test_user = User("Test","user","074040404","test@user.com") # new user
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
+
 if __name__ == '__main__':
     unittest.main()
+
+
         
