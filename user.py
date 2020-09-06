45dad71d134680a9ff9927b68574fc49982934ae
legionpunisher <mbuguamoses656@gmail.com>
2020-09-06 15:04:18 +0300

    created our first test on user to test to see that our objects are being initiated correctly

--- user.py
+++ user.py
@@ -4,7 +4,7 @@ class User:
     """
     user_list = []
     
-    def _init_(self,first_name,last_name,number,email):
+    def __init__(self,first_name,last_name,number,email):
     
         self.first_name = first_name
         self.last_name = last_name
--- user_test.py
+++ user_test.py
@@ -5,3 +5,22 @@ class TestUser (unittest.TestCase):
     """
     test class that defines test cases for the user class behaviour.
     """
+    #First test
+    def setUp(self):
+        """
+        setUp method to run before each test cases.
+        """
+        self.new_user = User("Moses","Mbugua","0790437510","mbuguamoses656@gmail.com")
+    def test_init(self):
+        """
+        test_init test case to test if the object is initialized properly
+        """
+        
+        self.assertEqual(self.new_user.first_name,"Moses")
+        self.assertEqual(self.new_user.last_name,"Mbugua")
+        self.assertEqual(self.new_user.phone_number,"0790437510")
+        self.assertEqual(self.new_user.email,"mbuguamoses656@gmail.com")
+        
+if __name__ == '__main__':
+    unittest.main()
+        
